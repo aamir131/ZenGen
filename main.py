adf600f3815e7397fab84ebff030d0eb468dc533
Aamir Soni <aamir@zencheck.ai>
2025-03-10 15:54:09 +0000

    update code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PromptEngine.PromptEngine import engine
 
 if __name__ == "__main__":
   customer_prompt_chain: list[dict] = load_convo_from_file(sys.argv[1])
-  customer_prompt = sys.argv[2] #Customer Prompt: The customer is asking: Generate an initial analysis based on the following information, and requirements:
+  customer_prompt = sys.argv[2]
   project_description = sys.argv[3]
   table_path = sys.argv[4]
   call_transcript_path = sys.argv[5]
@@ -18,7 +18,7 @@ if __name__ == "__main__":
   call_transcript = load_file(call_transcript_path)
 
   output_analysis_file = sys.argv[9]
-  output_other_data_file = sys.argv[10]
+  output_explanation_file = sys.argv[10]
   output_convo_file = sys.argv[11]
   
   generated_data, updated_prompt_chain = asyncio.run(engine.generate_content(
@@ -26,5 +26,8 @@ if __name__ == "__main__":
   ))
 
   write_to_file(output_analysis_file, generated_data.analysis)
-  write_to_file(output_other_data_file, generated_data.explanation)
+  write_to_file(output_explanation_file, generated_data.explanation)
   write_convo_to_file(output_convo_file, updated_prompt_chain)
+
+  
+  #python main.py ./test_data/customer_prompt_chain.json "What's the customer's main complaint?" "Analyzing customer complaints for telecom support" ./test_data/test_table.txt ./test_data/management_call.txt "test_topic" "test_textbox" "short" "analysis_output.txt" "explanation_file.txt" "promp_chain.json"
